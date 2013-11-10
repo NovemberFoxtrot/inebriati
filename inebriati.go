aa0b0059428217f73236bd17617502d0e071f2d7
NovemberFoxtrot <dothehustle@hush.com>
2013-11-10 21:39:57 +0900

    Update inebriati.go

--- inebriati.go
+++ inebriati.go
@@ -17,7 +17,7 @@ var (
 	StandardDrinks      = flag.Float64("drinks", 2.5, "How many drinks consumed")
 	BodyWeightKiloGrams = flag.Float64("weight", 70.0, "Body weight in kilograms")
 	DrinkingPeriodHours = flag.Float64("hours", 2.0, "How many hours it took to drink")
-	Gender              = flag.String("gender", "male", "Specify a gender of male or female")
+	Gender              = flag.String("gender", "male", "Specify male or female")
 )
 
 func main() {
