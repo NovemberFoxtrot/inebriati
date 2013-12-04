68c7069a8cd5ad83871a431ad657c445312e9e0d
NovemberFoxtrot <november@fox.trot>
2013-12-03 21:02:57 -0800

    rename

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/NovemberFoxtrot/inebriati/calc"
-)
-
-var (
-	StandardDrinks      = flag.Float64("drinks", 2.5, "How many drinks consumed")
-	BodyWeightKiloGrams = flag.Float64("weight", 70.0, "Body weight in kilograms")
-	DrinkingPeriodHours = flag.Float64("hours", 2.0, "How many hours it took to drink")
-	Gender              = flag.String("gender", "male", "Specify male or female")
+	"github.com/NovemberFoxtrot/inebriati/inebriati"
 )
 
 func main() {
+	drinks := flag.Float64("drinks", 2.5, "How many drinks consumed")
+	weight := flag.Float64("weight", 70.0, "Body weight in kilograms")
+	hours := flag.Float64("hours", 2.0, "How many hours it took to drink")
+	gender := flag.String("gender", "male", "Specify male or female")
+
 	flag.Parse()
 
-	i := inebriati.New(*StandardDrinks, *BodyWeightKiloGrams, *DrinkingPeriodHours, *Gender)
+	i := inebriati.New(*drinks, *weight, *hours, *gender)
 	i.Calc()
 
 	fmt.Println(i)
